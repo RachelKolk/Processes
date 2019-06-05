ac3b60678fb3d7a1ad0c9b6766aa5d3ca57e50bb
RachelKolk <rach.m.obrien@gmail.com>
2019-06-05 15:58:50 -0400

    ex1 coded

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,13 +2,28 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main(void)
 {
-    // Your code here
+    int r = 5;
+
+    pid_t pid = fork();
+
+    printf("pid is %d\n", pid);
+
+    if (pid == 0) {
+        printf("I am the child. My pid is: %d and the value of r is: %d\n", pid, r);
+        r = 10;
+        printf("Still the child, the value of r is now %d\n", r);
+    } else {
+        printf("I am the parent. My pid is: %d and the value of r is: %d\n", pid, r);
+        r = 2;
+        printf("Still the parent, the value of r is now %d\n", r);
+    }
 
     return 0;
 }
--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,10 @@
 
 int main(void)
 {
-    // Your code here 
+    FILE* fp;
+    fp = fopen("text.txt", w+);
+
     
+
     return 0;
 }
