5bae219eab06bb8f21a146a905378667e980b36d
RachelKolk <rach.m.obrien@gmail.com>
2019-06-06 19:19:55 -0400

    done with ex5 and ex6

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,7 +16,46 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
+    //a buffer that will hold the incoming data that is being written
+    char inbuf[MSGSIZE];
+    //a two element array to hold the read[0] and write[1] file descriptors
+    int p[2];
+
+    // we establish our pipe, passing it the above array so that it gets populated by
+    //the read and write file descriptors
+    if (pipe(p) < 0) {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+
+    // we create our fork
+    pid_t pid = fork();
+    printf("pid is %d\n", pid);
+
+    // we check to see if it's the child and if so it writes the messages
+    if (pid == 0) {
+        printf("I am the child...writing\n");
+
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+
+    // if it's the parent it reads/prints the messages
+    else {
+        int wait = waitpid(pid, NULL, 0);
+
+        printf("I am the parent...reading\n");
+
+        close(p[1]);
+
+        for (int i = 0; i < 3; i++) {
+            read(p[0], inbuf, MSGSIZE);
+            printf("%s\n", inbuf);
+        }
+        printf("Done.\n"); 
+
+    }
     
     return 0;
 }
